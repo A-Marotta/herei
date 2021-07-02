37f65ae072ef8215f67c33db3165731b3989f5ee
Karen cai <gigi.cai0310@gmail.com>
2021-07-02 20:09:13 +1000

    abstract mail sender to shared module

---
+++ email_notification/send.js
@@ -0,0 +1,32 @@
+const nodemailer = require("nodemailer");
+
+const sendEmail = async (email, subject, html) => {
+    try {
+        console.log(process.env.SERVICE);
+        console.log(process.env.USER);
+        console.log(process.env.PASS);
+
+        const transporter = nodemailer.createTransport({
+            host: process.env.SERVICE,
+            port: 587,
+            secure: false,
+            auth: {
+                user: process.env.EMAIL_USER,
+                pass: process.env.PASS,
+            },
+        });
+
+        await transporter.sendMail({
+            from: `"HereI" <process.env.EMAIL_USER>`,
+            to: email,
+            subject: subject,
+            html: html,
+        });
+        console.log("email sent sucessfully");
+    } catch (error) {
+        console.log("email not sent");
+        console.log(error);
+    }
+};
+
+module.exports = sendEmail;
